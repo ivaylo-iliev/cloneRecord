52f913b493299dba4f7e4b2475797d373b3cf90c
Ivaylo Iliev <ivailo.iliev@next-consult.com>
2021-08-29 13:11:54 +0300

    Fields and layout information parsing done.

--- force-app/main/default/lwc/cloneRecord/cloneRecord.js
+++ force-app/main/default/lwc/cloneRecord/cloneRecord.js
@@ -7,8 +7,11 @@ import {ShowToastEvent} from "lightning/platformShowToastEvent";
 export default class CloneRecord extends LightningElement {
 	@api recordId;
 	@api objectApiName;
+	@api showCalculatedFields = false;
+	@api fieldsToRemove = [];
 
-	layoutDetails = {};
+	layoutDetails = [];
+	fieldList = [];
 
 	formCanBeRendered = true;
 	showSpinner = false;
@@ -16,7 +19,12 @@ export default class CloneRecord extends LightningElement {
 	@wire(getRecordUi, {recordIds: '$recordId', layoutTypes: 'Full', modes: 'Edit'})
 	getRecordUiData({error, data}){
 		if(data){
-
+			console.log('layout information', JSON.parse(JSON.stringify(data)));
+			this.parseFieldDetails(data);
+			if(this.fieldList !== null && this.fieldList !== undefined && this.fieldList.length > 0){
+				this.parseLayoutData(data);
+			}
+			
 		}
 
 		if(error){
@@ -36,5 +44,55 @@ export default class CloneRecord extends LightningElement {
 
 	}
 
+	parseLayoutData(data){
+		let sections = [];
 	
+		for(const[key, value] of Object.entries(data.layouts[this.objectApiName])){
+			let layoutDetails = value;
+
+			layoutDetails.Full.Edit.sections.forEach(item => {
+				let section = {};
+				console.log('section: ', JSON.parse(JSON.stringify(item)));
+				section.id = item.id;
+				section.label = item.heading;
+				section.columns = item.columns;
+				section.collpsible = item.collpsible;
+				section.rows = [];
+			
+				item.layoutRows.forEach(row => {
+					let layoutRow = [];
+				    row.layoutItems.forEach(layoutItem => {
+						let field = {};
+						layoutItem.layoutComponents.forEach(layoutComponent => {
+							if (layoutComponent?.apiName && this.fieldList.includes(layoutComponent.apiName)) {						
+								field = {
+									apiName: layoutComponent.apiName,
+									cssClass: `slds-size_1-of-${item.columns}`
+								};
+								layoutRow.push(field);
+							}
+						});
+					});	
+					section.rows.push(layoutRow);
+				});
+
+				sections.push(section);
+            });
+		}
+
+		console.log('sections: ', sections);
+		this.layoutDetails = sections;
+	}
+
+	parseFieldDetails(data){
+		for(const[key, value] of Object.entries(data.objectInfos[this.objectApiName].fields)) {
+			if(this.showCalculatedFields){
+				this.fieldList.push(value.apiName);
+			} else if(!value.calculated){
+				this.fieldList.push(value.apiName);
+			}				
+		}
+
+		console.log(this.fieldList);
+	}
 }
